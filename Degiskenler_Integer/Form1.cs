d87f5048f4ad432c36a064ba83030353133d4818
Mustafa Emre Ilhan <mustafaemreilhan1903@gmail.com>
2021-05-25 09:45:26 +0300

    integer değişken tanımladık ve bunu stringe çevirdik

--- Degiskenler_Integer/Form1.cs
+++ Degiskenler_Integer/Form1.cs
@@ -16,5 +16,12 @@ namespace Degiskenler_Integer
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int sayi;
+            sayi = 24;
+            label1.Text = sayi.ToString();
+        }
     }
 }
